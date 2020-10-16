7dc0dac153baaad83526833260be66e2a2888487
jieliu <jieliu.org@gmail.com>
2020-10-16 16:11:49 +0800

    init

---
+++ git.plugin.zsh
@@ -0,0 +1,43 @@
+#
+# Aliases
+# (sorted alphabetically)
+#
+
+# g clone url; g init
+alias g='git'
+
+# ga . ; ga xxx
+alias ga='git add'
+
+# gb ; gb dev; gb -r
+alias gb='git branch'
+
+# gc 'commit message'
+alias gc='git commit -v -a'
+
+# go -b xxx ; go dev; go master
+alias go='git checkout'
+
+# gl 
+alias gl='git pull'
+
+# glog
+alias glog='git log --oneline --decorate --graph --all'
+
+# gm xxx ; gm --no-ff xxx 
+alias gm='git merge'
+
+alias gp='git push'
+
+# grebase  xxxx
+alias grebase='git rebase -i'
+
+# grmc xxxx
+alias grmc='git rm --cached'
+
+alias gr='git remote -v'
+
+alias gs='git status -sb'
+
+# gt ; gt xxx
+alias gt='git tag'
